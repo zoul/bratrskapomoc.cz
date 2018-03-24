db3ce2fa43e65af83fb1041f8c8f992a9e343e65
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2018-03-24 22:02:32 +0100

    First rough version with a working map

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,10 @@ module Main exposing (..)
 import Html exposing (Html)
 import Victim exposing (..)
 import Http
+import Maps
+import Maps.Marker as Marker
+import Maps.Map as Map
+import Maps.Geo
 
 
 main =
@@ -16,12 +20,13 @@ main =
 
 type Model
     = Loading
-    | Loaded (List Victim)
+    | Loaded { map : Maps.Model Msg, victims : List Victim }
     | Error String
 
 
 type Msg
     = ReceiveResponse (Result Http.Error String)
+    | MapsMsg (Maps.Msg Msg)
 
 
 loadVictims : Cmd Msg
@@ -34,21 +39,46 @@ init =
     ( Loading, loadVictims )
 
 
+buildMap : List Victim -> Maps.Model Msg
+buildMap victims =
+    let
+        initialLocation =
+            Maps.Geo.latLng 49.5 17
+
+        pins =
+            victims
+                |> List.filterMap .incidentLatLong
+                |> List.map (uncurry Maps.Geo.latLng)
+    in
+        Maps.defaultModel
+            |> Maps.updateMap (Map.setZoom 6 >> Map.moveTo initialLocation)
+            |> Maps.updateMarkers (\_ -> List.map Marker.create pins)
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        ReceiveResponse result ->
+    case ( msg, model ) of
+        ( ReceiveResponse result, _ ) ->
             case result of
                 Ok data ->
                     case parseVictims data of
                         Just victims ->
-                            ( Loaded victims, Cmd.none )
+                            ( Loaded { map = buildMap victims, victims = victims }, Cmd.none )
 
                         Nothing ->
                             ( Error "CSV parse error", Cmd.none )
 
                 Err e ->
                     ( Error (toString e), Cmd.none )
+
+        ( MapsMsg msg, Loaded model ) ->
+            model.map
+                |> Maps.update msg
+                |> Tuple.mapFirst (\map -> Loaded { model | map = map })
+                |> Tuple.mapSecond (Cmd.map MapsMsg)
+
+        _ ->
+            ( Error "Invalid state", Cmd.none )
 
 
 view : Model -> Html Msg
@@ -60,5 +90,5 @@ view model =
         Error e ->
             Html.text ("Error: " ++ e)
 
-        Loaded victims ->
-            Html.text ("Victims: " ++ toString (List.length victims))
+        Loaded model ->
+            Maps.view model.map |> Maps.mapView MapsMsg
--- src/Victim.elm
+++ src/Victim.elm
@@ -4,6 +4,10 @@ import Csv exposing (Csv)
 import Csv.Decode exposing (..)
 
 
+type alias LatLong =
+    ( Float, Float )
+
+
 type alias Victim =
     { name : String
     , birthDate : String
@@ -11,9 +15,29 @@ type alias Victim =
     , deathAge : Maybe Int
     , incidentDate : String
     , incidentPlace : String
-    , incidentGPS : String
+    , incidentLatLong : Maybe LatLong
     , incidentType : String
     }
+
+
+decodeLatLong : String -> Result String LatLong
+decodeLatLong str =
+    let
+        toList =
+            identity
+                -- "50.0814253N", "14.4876106E"
+                >> String.split ", "
+                -- "50.0814253", "14.4876106"
+                >> List.map (String.dropRight 1)
+                -- 50.0814253, 14.4876106
+                >> List.filterMap (Result.toMaybe << String.toFloat)
+    in
+        case toList str of
+            [ x, y ] ->
+                Ok ( x, y )
+
+            _ ->
+                Err "Failed to parse map coordinates"
 
 
 decodeVictim : Decoder (Victim -> a) a
@@ -32,7 +56,7 @@ decodeVictim =
                 |> andMap (next (maybe int))
                 |> andMap (next str)
                 |> andMap (next str)
-                |> andMap (next str)
+                |> andMap (next (maybe decodeLatLong))
                 |> andMap (next str)
             )
 
@@ -44,6 +68,8 @@ parseVictims str =
             not << String.isEmpty << .name
 
         result =
-            Csv.parse str |> decode decodeVictim |> Result.map (List.filter hasName)
+            Csv.parse str
+                |> decode decodeVictim
+                |> Result.map (List.filter hasName)
     in
         Result.toMaybe result
